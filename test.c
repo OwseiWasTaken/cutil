2f89baadb63851898dc84a95fdb00042971915b0
owsei <owseiwastaken@gmail.com>
2022-12-14 16:37:34 -0300

    test _Print func

--- test.c
+++ test.c
@@ -1,12 +1,7 @@
 #include "cutil.h"
 
 int main(void) {
-	StartChTerm(NULL);
-
-	// write NULL
-	kbkey k = GetChId();
-	printf("is up: %s\n", k==KEY_up?"true":"false");
-	printf("is down: %s\n", k==KEY_down?"true":"false");
-
-	StopChTerm(NULL);
+	point TermSize = GetTerminalSize();
+	_Print_Point(TermSize);
 }
+
