fdfe1a7ed5a07a041207681d5e79de5c783998f6
owsei <owseiwastaken@gmail.com>
2023-01-02 00:12:30 -0300

    rename /util to /sidekick, updated walkin.c

--- examples/walkin.c
+++ examples/walkin.c
@@ -2,32 +2,29 @@
 
 int main(void) {
 	StartChTerm(NULL);
-	point TermSize = GetTerminalSize();
-	int y = 2, x = 2;
+	upoint TermSize = GetTerminalSize();
+	TermSize.y--;
+	TermSize.x--;
+	uint y = 1, x = 1;
 	long k = 0;
 
 	while (k!=KEY_q){
-		move(y, x); // move
+		move(TermSize.y+1, 0);
+		printf("%u, %u", y, x);
+		move(y+1, x+1); // move
 		flush(); // and show
 
 		k = GetChId();
 		// status line
-		{
-			move(TermSize.y, 0);
-			printf("udlr: %i%i%i%i @ %i,%i",
-				k==KEY_up, k==KEY_down, k==KEY_left, k==KEY_right,
-				y, x
-			);
-		}
 		switch (k) {
 			case KEY_up:
-				if (y-1) y--;
+				if (y) y--;
 				break;
 			case KEY_down:
 				if (y < TermSize.y) y++;
 				break;
 			case KEY_left:
-				if (x-1) x--;
+				if (x) x--;
 				break;
 			case KEY_right:
 				if (x < TermSize.x) x++;
