2ab96519382fde8d96399af5093edc0f77c592b2
Pedro Manse <pedro.manse@dmk3.com.br>
2023-08-07 00:23:41 -0300

    standardazing files

--- examples/linked.c
+++ examples/linked.c
@@ -7,11 +7,13 @@ int *MakeObject() {
 	return info;
 }
 
-void print_node(linked_list *list, int index, void* obj) {
+void print_node(LinkedList *list, int index, void* obj) {
+	(void)list;
 	printf("%d: <%d>\n", index, *(int*)obj);
 }
 
-void remove_odd(linked_list *list, int index, void* obj) {
+void remove_odd(LinkedList *list, int index, void* obj) {
+	(void)list;
 	int info = *(int*)obj;
 	if ( (info%2) == 1 ) {
 		list_remove(list, index, true);
@@ -19,7 +21,7 @@ void remove_odd(linked_list *list, int index, void* obj) {
 }
 
 int main() {
-	linked_list *list = list_alloc();
+	LinkedList *list = list_alloc();
 	assert(list_append(list, MakeObject()));
 
 	for (int i = 0; i < 20; i++) {
@@ -31,18 +33,18 @@ int main() {
 	//assert(list_remove(list, 5, true));
 	//list_iter(list, print_node);
 
+	//list_iter(list, print_node);
+	//list_iter(list, remove_odd);
 	list_iter(list, print_node);
-	list_iter(list, remove_odd);
-	list_iter(list, print_node);
-	//for (int i = 0; i<list->len; i++) {
-	//	int info = *(int*)list_get(list, i);
-	//	//printf("%d: <%d> [%%2:%d]\n", i, info, (info%2) == 1);
-	//	if ( (info%2) == 1 ) {
-	//		list_remove(list, i, true);
-	//		i--;
-	//	}
-	//}
-	//list_iter_node(list, node_iter_print);
+	for (int i = 0; i<list->len; i++) {
+		int info = *(int*)list_get(list, i);
+		//printf("%d: <%d> [%%2:%d]\n", i, info, (info%2) == 1);
+		if ( (info%2) == 1 ) {
+			list_remove(list, i, true);
+			i--;
+		}
+	}
+	list_iter_node(list, node_iter_print);
 
 
 	list_free(list, true);
